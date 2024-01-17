70b57ff03440b793a89b06e1945814125e720ec8
gastbr <71907959+gastbr@users.noreply.github.com>
2024-01-17 22:23:43 +0000

    Add files via upload

---
+++ CaraCruz.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaExamen
+{
+    internal public static class CaraCruz
+    {
+
+        int empates = 0;
+        private static int field;
+
+        public static event EventHandler Event;
+
+        static CaraCruz()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static int Property
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public static int Property1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        int lanzaMoneda(String jugador)
+        {
+            int resCaraCruz;
+            resCaraCruz = floor(random(0, 2));
+            if (resCaraCruz == 0)
+            {
+                print(jugador + ": CARA");
+            }
+            else if (resCaraCruz == 1)
+            {
+                print(jugador + ": CRUZ");
+            }
+            else
+            {
+                print(jugador + ": ERROR");
+            }
+            return resCaraCruz;
+        }
+
+        void caraCruz(String jug0, String jug1, String jug2)
+        {
+            int[] res = new int[3];
+
+            res[0] = lanzaMoneda(jug0);
+            print(" | ");
+            res[1] = lanzaMoneda(jug1);
+            print(" | ");
+            res[2] = lanzaMoneda(jug2);
+            print(" | ");
+
+            if (res[0] + res[1] + res[2] == 0 || res[0] + res[1] + res[2] == 3)
+            {
+                print("EMPATE");
+                empates++;
+            }
+            else if (res[0] == res[1] && res[0] != res[2])
+            {
+                print("Gana " + jug2);
+            }
+            else if (res[0] == res[2] && res[0] != res[1])
+            {
+                print("Gana " + jug1);
+            }
+            else if (res[1] == res[2] && res[0] != res[1])
+            {
+                print("Gana " + jug0);
+            }
+            else
+            {
+                print("ERROR");
+            }
+
+            println();
+        }
+
+        void cienCaraCruz(String jug0, String jug1, String jug2)
+        {
+            empates = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                print(i + 1 + ": ");
+                caraCruz(jug0, jug1, jug2);
+            }
+
+            println();
+            println("Porcentaje de empates: " + empates + "%");
+        }
+
+
+    }
+
+    internal public static class CopyOfCaraCruz
+    {
+
+        int empates = 0;
+        private static int field;
+
+        public static event EventHandler Event;
+
+        static CopyOfCaraCruz()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static int Property
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public static int Property1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        int lanzaMoneda(String jugador)
+        {
+            int resCaraCruz;
+            resCaraCruz = floor(random(0, 2));
+            if (resCaraCruz == 0)
+            {
+                print(jugador + ": CARA");
+            }
+            else if (resCaraCruz == 1)
+            {
+                print(jugador + ": CRUZ");
+            }
+            else
+            {
+                print(jugador + ": ERROR");
+            }
+            return resCaraCruz;
+        }
+
+        void caraCruz(String jug0, String jug1, String jug2)
+        {
+            int[] res = new int[3];
+
+            res[0] = lanzaMoneda(jug0);
+            print(" | ");
+            res[1] = lanzaMoneda(jug1);
+            print(" | ");
+            res[2] = lanzaMoneda(jug2);
+            print(" | ");
+
+            if (res[0] + res[1] + res[2] == 0 || res[0] + res[1] + res[2] == 3)
+            {
+                print("EMPATE");
+                empates++;
+            }
+            else if (res[0] == res[1] && res[0] != res[2])
+            {
+                print("Gana " + jug2);
+            }
+            else if (res[0] == res[2] && res[0] != res[1])
+            {
+                print("Gana " + jug1);
+            }
+            else if (res[1] == res[2] && res[0] != res[1])
+            {
+                print("Gana " + jug0);
+            }
+            else
+            {
+                print("ERROR");
+            }
+
+            println();
+        }
+
+        void cienCaraCruz(String jug0, String jug1, String jug2)
+        {
+            empates = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                print(i + 1 + ": ");
+                caraCruz(jug0, jug1, jug2);
+            }
+
+            println();
+            println("Porcentaje de empates: " + empates + "%");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Presionar 1 para Cara o cruz, 2 para Triángulo o 3 para Notas. 0 para salir:");
+            Console.WriteLine();
+
+            char tecla = (char)Console.ReadKey().Key;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            switch (tecla)
+            {
+                case '1':
+                    Console.WriteLine("Cara o cruz: José vs. Pepe vs. Juan.");
+                    
+
+
+                    Console.WriteLine();
+                    Console.WriteLine("Presionar 1 para Cara o cruz, 2 para Triángulo o 3 para Notas. 0 para salir.");
+                    Console.WriteLine();
+
+                    break;
+                case '2':
+                    Console.WriteLine("Ha elegido la opción 2");
+                    break;
+                case '3':
+                    Console.WriteLine("Ha elegido la opción 3");
+                    break;
+                case '0':
+                    Console.WriteLine("Ha elegido la opción 4");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+        }
+    }
+}
